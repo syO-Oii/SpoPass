094d7bf71f7d0e7dff110017f9f65262be77ca29
syO-Oii <as15421@naver.com>
2023-11-03 18:34:53 +0900

    세팅

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+import java.util.Date;
+
+public class Main {
+
+    //* 회원 번호 *//
+    private Integer UserNum;
+    //* 카드 번호 **/
+    private Integer CardNum;
+    private String UserName;
+    private String Subject;
+    private Date StartDate;
+    private Date EndDate;
+
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
